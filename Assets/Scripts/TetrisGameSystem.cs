749b8a689a71b943559bebd5849f84476fc11e11
Claudio Alberto Faria Gonçalves <Claudio Alberto Faria Gonalves>
2016-02-21 06:31:58 -0300

    movimentando os blocos horizontalmente

--- Assets/Scripts/TetrisGameSystem.cs
+++ Assets/Scripts/TetrisGameSystem.cs
@@ -32,6 +32,7 @@ public class TetrisGameSystem : MonoBehaviour {
                 blocks[j, i] = Instantiate(blockModel, new Vector2(j+(j*0.1f), i+(i*0.1f)), Quaternion.identity) as GameObject;
             }
         }
+        
         gameScreen[5, 10] = 1;
         gameScreen[5, 11] = 1;
         gameScreen[4, 10] = 1;
@@ -57,6 +58,7 @@ public class TetrisGameSystem : MonoBehaviour {
     //Faz os blocos descerem
     void MoveBlockToDown()
     {
+        bool OK = MoveBlockToDownOK();
         int i, j;
         for(i=0; i < height; i++)
         {
@@ -64,24 +66,115 @@ public class TetrisGameSystem : MonoBehaviour {
             {
                 if (gameScreen[j, i] == 1)
                 {
-                    if (i > 0)
+                    if (OK)
                     {
-                        if (gameScreen[j, i - 1] == -1)
-                        {
-                            gameScreen[j, i] = -1;
-                            gameScreen[j, i - 1] = 1;
-                        }
-                        else
-                            gameScreen[j, i] = 0;
+                        gameScreen[j, i] = -1;
+                        gameScreen[j, i - 1] = 1;
                     }
-                    else
-                        gameScreen[j, i] = 0;
+                    else{ gameScreen[j, i] = 0; }
+                    
                 }
             }
         }
     }
+    //Verifica se os blocos podem descer ou não
+    bool MoveBlockToDownOK()
+    {
+        int i, j;
+        for (i = 0; i < height; i++)
+        {
+            for (j = 0; j < width; j++)
+            {
+                if (gameScreen[j, i] == 1)
+                {
+                    if (i > 0)
+                    {
+                        if (gameScreen[j, i - 1] == 0)
+                        {
+                            return false;
+                        }
+                    }
+                    else { return false; }
+                        
+                }
+            }
+        }
+        return true;
+    }
+    //verifica se é possivel mover os blocos
+    bool MoveBlockHorizontalOK(int direction)
+    { 
+        int i, j;
+        int initialJ;
+        if (direction == -1)
+        {
+            initialJ = 0;
+        }
+        else { initialJ = width-1; }
+        for (i = 0; i < height; i++)
+        {
+            j = initialJ;
+            while (j >= 0 && j < width)
+            {
+                if (gameScreen[j,i]==1)
+                {
+                    if ((j + direction) < 0 || j >= width)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        if (gameScreen[j + direction, i] == 0)
+                            return false;
+                    }
+                    
+                }
+                
+            }
+            j = j - direction;
+        }
+        return true;
+    }
+    //Move os blocos horizontalmente
+    void MoveBlockHorizontal(int direction)
+    {
+        int i, j;
+        int initialJ;
+        bool OK = MoveBlockHorizontalOK(direction);
+        if (direction == -1)
+        {
+            initialJ = 0;
+        }
+        else { initialJ = width - 1; }
+        for (i = 0; i < height; i++)
+        {
+            j = initialJ;
+            while (j >= 0 && j < width)
+            {
+                if (gameScreen[j, i] == 1)
+                {
+                    if(OK)
+                    {
+                        gameScreen[j, i] = -1;
+                        gameScreen[j + direction, i] = 1;
+                    }
+                }
+                j = j - direction;
+            }
+            
+        }
+        
+    }
 
-
+    void Controller()
+    {
+        if(Input.GetKeyDown(KeyCode.A)){
+            MoveBlockHorizontal(-1);
+        }
+        if (Input.GetKeyDown(KeyCode.D)){
+            MoveBlockHorizontal(1);
+        }
+    }
     void Update()
     {
         timeIntervalToMoveBlockToDown += Time.deltaTime;
@@ -91,5 +184,6 @@ public class TetrisGameSystem : MonoBehaviour {
             timeIntervalToMoveBlockToDown = 0;
         }
         SetColorInBlocks();
+        Controller();
     }
 }
